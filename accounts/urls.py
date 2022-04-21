b68fcc8a3fb50efe3906c5b6b4c54940e0438ecd
amit1870 <amitxvf@gmail.com>
2022-04-21 13:30:26 +0530

    URL update

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('', views.register, name='register'),
-    path('siyaram', views.siyaram, name='siyaram'),
+    path('', views.siyaram, name='siyaram'),
+    path('register', views.register, name='register'),
     path('success', views.success, name='success'),
     path('login/', views.account_login, name='login'),
-    path('logout', views.account_logout, name='logout'),
+    path('logout/', views.account_logout, name='logout'),
     path('activate/<str:hex_code>', views.activate, name='activate'),
 
 ]
