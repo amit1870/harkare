3c2c114e47f8fcba06977872c805fff4ff8d6404
amit1870 <amitxvf@gmail.com>
2022-04-23 16:41:46 +0530

    Harkare with MP3 feature released

--- ramjee/models.py
+++ ramjee/models.py
@@ -6,3 +6,6 @@ class Harkare(models.Model):
     manushya = models.ForeignKey(Manushya, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, blank=True)
     url = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
