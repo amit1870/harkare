8cd4d6c7c93bccc62e2a43576b5f61f4cebd4f27
amit1870 <amitxvf@gmail.com>
2022-05-02 17:59:00 +0530

    added category for mp3

--- ramjee/models.py
+++ ramjee/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 from accounts.models import Manushya
 
+VIBHAKTI = [
+    ('KH', 'Kahani'),
+    ('SH', 'Shashtra'),
+    ('VD', 'Ved'),
+    ('PN', 'Puran'),
+    ('LK', 'Lokmanya'),
+    ('KY', 'Kavya'),
+    ('PY', 'Padya'),
+    ('PM', 'Prem'),
+    ('BR', 'Gyan'),
+    ('SR', 'Siyaram'),
+]
 
 class Harkare(models.Model):
     manushya = models.ForeignKey(Manushya, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, blank=True)
     url = models.TextField(blank=True)
+    vibhakti = models.CharField(max_length=2, choices=VIBHAKTI, default='SR')
 
     def __str__(self):
         return self.name
