dde407d99f433073a9193dbcb9689860694e7373
amit1870 <amitxvf@gmail.com>
2022-05-04 13:46:52 +0530

    Minor tweak

--- ramjee/urls.py
+++ ramjee/urls.py
@@ -11,7 +11,7 @@ app_name = 'ramjee'
 urlpatterns = [
     path('harkare/upload/', views.upload_harkara, name='upload'),
     path('harkare/donation/', views.donation, name='donation'),
-    path('harkare/limit/', views.show_limitation, name='limitation'),
+    path('harkare/limits/', views.limitations, name='limitations'),
     path('harkare/', views.list_harkare, name='harkare'),
     # path('', include(router.urls)),
 
